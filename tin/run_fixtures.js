b16c95c7a1e842441d14f28715e0857981cbf4ed
Eli Goodman <eli.m.goodman@gmail.com>
2011-04-10 18:19:54 -0400

    remove?

--- tin/run_fixtures.js
+++
@@ -1,9 +0,0 @@
-var config = require("./config");
-var model = require('./model'); 
-var fixtures = require('./fixtures'); 
-
-var config = config.createConfig(process.argv[2]);
-
-var models = model.generateModels(config.app_dir);
-
-fixtures.runFixtures(models, config);
